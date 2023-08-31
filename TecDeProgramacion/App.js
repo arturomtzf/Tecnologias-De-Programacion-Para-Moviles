3e9a275c9deb14d156d41d92eaaa5804b3553be8
Arturo Mtz <102838956+arturomtzf@users.noreply.github.com>
2023-08-31 08:53:35 -0600

    Fasmilia

--- TecDeProgramacion/App.js
+++ TecDeProgramacion/App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Image, Dimensions, TextInput } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
 import Login from "./src/screens/Login";
+import { Person } from "./src/components/Person/Person";
 
 export default function App() {
-    const { height } = Dimensions.get("screen");
+    const DATA = [
+        { id: 0, name: "Juan", lastName: "Perez" },
+        { id: 1, name: "Pedro", lastName: "Rivera" },
+        { id: 2, name: "Maria", lastName: "Garcia" },
+        { id: 3, name: "Jose", lastName: "Dominguez" },
+        { id: 4, name: "Luis", lastName: "Manriquez" },
+        { id: 5, name: "Ana", lastName: "Martinez" },
+        { id: 6, name: "Luisa", lastName: "Juarez" },
+    ];
 
     return (
         <View style={styles.container}>
-            <Login />
+            {/* <Login /> */}
+            <FlatList
+                data={DATA}
+                ItemSeparatorComponent={<Text>Separador</Text>}
+                keyExtractor={({id}) => id} //Desestructuramos el id de cada item
+                renderItem={(
+                    { item: { name, lastName }, index } // Para destructurar item
+                ) => <Person name={name} lastName={lastName} index={index} />}
+            />
+            <StatusBar style="auto" />
         </View>
     );
 }
---
+++ TecDeProgramacion/src/components/Person/Person.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const Person = ({name, lastName, index}) => {
+    return (
+        <View style={{ borderWidth: 1, borderColor: index % 2 === 0 ? "red" : "blue" }}>
+            <Text style={{ fontSize: 40 }}>{name}</Text>
+            <Text style={{ fontSize: 40 }}>{lastName}</Text>
+        </View>
+    );
+};
