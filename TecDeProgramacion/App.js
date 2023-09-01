fdfc92f1cdade93f035b0cf9dd92dce1421d9e34
Arturo Mtz <arturo_mtzfts@hotmail.com>
2023-08-31 22:58:31 -0600

    Imagenes y FlatList

--- TecDeProgramacion/App.js
+++ TecDeProgramacion/App.js
@@ -6,10 +6,10 @@ import { Person } from "./src/components/Person/Person";
 export default function App() {
     const DATA = [
         { id: 0, name: "Juan", lastName: "Perez", image: "https://picsum.photos/200" },
-        { id: 1, name: "Pedro", lastName: "Rivera", image: "https://picsum.photos/201" },
+        { id: 1, name: "Presiona", lastName: "Me", image: "https://picsum.photos/201" },
         { id: 2, name: "Maria", lastName: "Garcia", image: "https://picsum.photos/202" },
-        { id: 3, name: "Jose", lastName: "Dominguez", image: "https://picsum.photos/203" },
-        { id: 4, name: "Luis", lastName: "Manriquez", image: "https://picsum.photos/204" },
+        { id: 3, name: "Jose", lastName: "Gomez", image: "https://picsum.photos/203" },
+        { id: 4, name: "Eder", lastName: "Ruiz", image: "https://picsum.photos/204" },
         { id: 5, name: "Ana", lastName: "Martinez", image: "https://picsum.photos/205" },
         { id: 6, name: "Luisa", lastName: "Juarez", image: "https://picsum.photos/206" },
     ];
@@ -19,11 +19,11 @@ export default function App() {
             {/* <Login /> */}
             <FlatList
                 data={DATA}
-                ItemSeparatorComponent={<Text>Separador</Text>}
+                ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
                 keyExtractor={({ id }) => id} //Desestructuramos el id de cada item
                 renderItem={(
                     { item: { name, lastName, image }, index } // Para destructurar item
-                ) => <Person name={name} lastName={lastName} index={index} imagen={image}/>}
+                ) => <Person name={name} lastName={lastName} index={index} imagen={image} />}
             />
             <StatusBar style="auto" />
         </View>
--- TecDeProgramacion/src/components/Person/Person.jsx
+++ TecDeProgramacion/src/components/Person/Person.jsx
@@ -1,12 +1,40 @@
 import React from "react";
-import { Text, TouchableOpacity, Image } from "react-native";
+import { Text, TouchableOpacity, Image, Linking } from "react-native";
 
-export const Person = ({name, lastName, index, imagen}) => {
+export const Person = ({ name, lastName, index, imagen }) => {
     return (
-        <TouchableOpacity style={{ borderWidth: 1, borderColor: index % 2 === 0 ? "red" : "blue" }}>
-            <Image source={'https://picsum.photos/200'}/>
-            <Text style={{ fontSize: 40 }}>{name}</Text>
-            <Text style={{ fontSize: 40 }}>{lastName}</Text>
+        <TouchableOpacity
+            style={[styles.touchable, { borderColor: index % 2 === 0 ? "red" : "blue" }]}
+            onPress={() => Linking.openURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ")}
+        >
+            <Image
+                source={{
+                    uri: imagen,
+                }}
+                style={styles.imagen}
+            />
+            <Text style={styles.texto}>{name}</Text>
+            <Text style={styles.texto}>{lastName}</Text>
         </TouchableOpacity>
     );
+};
+
+const styles = {
+    imagen: {
+        width: 85,
+        height: 85,
+        borderRadius: 50,
+        borderWidth: 1,
+        borderColor: "gray",
+    },
+    touchable: {
+        borderWidth: 1,
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 20,
+    },
+    texto: {
+        fontSize: 40,
+        marginLeft: 10,
+    },
 };
