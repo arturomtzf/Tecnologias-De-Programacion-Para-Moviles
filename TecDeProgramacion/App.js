e0eb961c6134c8aa69432664c2a07a98608d3c13
Arturo Mtz <102838956+arturomtzf@users.noreply.github.com>
2023-09-22 09:01:59 -0600

    useEffect y peticion a API

--- TecDeProgramacion/App.js
+++ TecDeProgramacion/App.js
@@ -1,13 +1,37 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Image, Dimensions, TextInput } from "react-native";
-import Login from "./src/screens/Login";
+import { StyleSheet, View, Text, FlatList } from "react-native";
+import { useEffect, useState } from "react";
 
 export default function App() {
-    const { height } = Dimensions.get("screen");
+    const [characters, setCharacters] = useState([]);
+    const getApiData = async () => {
+        try {
+            const response = await fetch("https://rickandmortyapi.com/api/character");
+            const data = await response.json();
+            setCharacters(data.results);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        getApiData();
+    }, []); // Unicamente se ejecuta cuando el valor de input cambia
 
     return (
         <View style={styles.container}>
-            <Login />
+            <Text style={{ fontSize: 30 }}>Caracteres</Text>
+            <FlatList
+                data={characters}
+                ItemSeparatorComponent={() => <View ></View>}
+                renderItem={({ item }) => (
+                    <View>
+                        <Text>{item.id}</Text>
+                        <Text>{item.name}</Text>
+                        <Text>{item.gender}</Text>
+                    </View>
+                )}
+            ></FlatList>
         </View>
     );
 }
