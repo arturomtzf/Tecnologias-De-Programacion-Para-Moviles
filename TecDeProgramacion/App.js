c56041697610f7d76f98f3a5a80ec71b8a8d4579
Arturo Mtz <102838956+arturomtzf@users.noreply.github.com>
2023-09-28 08:03:51 -0600

    GOD Eder

--- TecDeProgramacion/App.js
+++ TecDeProgramacion/App.js
@@ -1,34 +1,61 @@
-import Constants from "expo-constants";
-import { StyleSheet, View, StatusBar } from "react-native";
-import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar as ReactStatus,
+} from "react-native";
+// import "react-native-gesture-handler";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import Login from "./src/screens/Login";
-import Home from "./src/screens/Home";
-import CharacterInfo from "./src/screens/CharacterInfo";
-
-// Tarea:  De la tarea que hicimos con los personajes de Rick and Morthy. Hacer una propiedad de ver personaje en cada card, y cuando le den click que los mnadne a una pantalla donde se muestre ***TODA*** la info del personaje.
+import { SafeAreaView } from "react-native-safe-area-context";
+import LoginScreen from "./src/screens/LoginScreen";
+import HomeScreen from "./src/screens/HomeScreen";
 
 const Stack = createStackNavigator();
+// const { Navigator, Screen } = Stack;
+// const BottomStack = createStackNavigator();
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <View style={styles.container}>
-                <Stack.Navigator initialRouteName="TheRichAndMorhyAPI">
-                    <Stack.Screen name="TheRichAndMorhyAPI" component={Home} />
-                    <Stack.Screen name="Login" component={Login} />
-                    <Stack.Screen name="Character" component={CharacterInfo} />
-                </Stack.Navigator>
-            </View>
-            <StatusBar />
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <SafeAreaView style={styles.container}>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#ce8455",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+        <StatusBar
+          style="auto"
+          animated={true}
+          // backgroundColor="#e0aeff"
+          // hidden={true}
+        />
+      </SafeAreaView>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-    },
+  container: {
+    // flex: 1,
+    // backgroundColor: "#aa76ff",
+    // alignItems: "center",
+    // justifyContent: "center",
+    width: Dimensions.get("screen").width,
+    height: Dimensions.get("screen").height,
+    // paddingTop: ReactStatus.currentHeight,
+  },
 });
---
+++ TecDeProgramacion/src/screens/HomeScreen.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button, Text, View } from "react-native";
+
+const HomeScreen = ({ navigation }) => {
+  console.log(navigation);
+  const { canGoBack, goBack } = navigation;
+  return (
+    <View>
+      <Button
+        onPress={() => {
+          navigation.navigate("Login", { name: "Arturo Martinez Fuentes" });
+        }}
+        title="Login"
+      />
+      <Button
+        onPress={() => navigation.goBack()}
+        title="Go back"
+        disabled={!canGoBack()}
+      />
+    </View>
+  );
+};
+
+export default HomeScreen;
