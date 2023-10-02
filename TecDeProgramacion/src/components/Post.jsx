29511bf004626eeedb450e1155bfb0ecebbccd80
Arturo Mtz <arturo_mtzfts@hotmail.com>
2023-10-02 16:06:42 -0600

    Para fhought

---
+++ TecDeProgramacion/src/components/Post.jsx
@@ -0,0 +1,87 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { MaterialIcons, AntDesign, Feather } from "@expo/vector-icons";
+
+export default function Post({ profilePic, name, image, caption, likes }) {
+    const [isLiked, setIsLiked] = useState(false);
+    const [likesCount, setLikesCount] = useState(likes);
+    const like = () => {
+        setIsLiked(!isLiked);
+        isLiked ? setLikesCount(likesCount - 1) : setLikesCount(likesCount + 1);
+    };
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={{ uri: profilePic }} style={styles.profilePic} />
+                <Text style={styles.userName}>{name}</Text>
+                <MaterialIcons name="verified" size={20} color="#0394f6" />
+            </View>
+            <Image source={{ uri: image }} style={styles.image} />
+            <View style={styles.options}>
+                <View style={styles.left}>
+                    <TouchableOpacity onPress={() => like()}>
+                        {isLiked ? (
+                            <AntDesign name="heart" size={28} color="red" />
+                        ) : (
+                            <AntDesign name="hearto" size={28} color="white" />
+                        )}
+                    </TouchableOpacity>
+                    <AntDesign name="message1" size={28} color="white" />
+                    <Feather name="send" size={28} color="white" />
+                </View>
+                <AntDesign name="save" size={28} color="white" />
+            </View>
+            <Text style={styles.likes}>Liked by <Text style={{fontWeight: "800"}}>arturomtzf</Text> and {likesCount - 1} others</Text>
+            <Text style={styles.caption}>
+                <Text style={{fontWeight: "800"}}>{name}</Text> {caption}
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginBottom: 15,
+        marginTop: 8,
+    },
+    header: {
+        flexDirection: "row",
+        alignItems: "center",
+        margin: 5,
+    },
+    profilePic: {
+        width: 40,
+        height: 40,
+        borderRadius: 40,
+        marginRight: 10,
+    },
+    userName: {
+        color: "white",
+        fontWeight: "700",
+        marginRight: 5,
+    },
+    image: {
+        width: "100%",
+        height: 450,
+    },
+    options: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        margin: 5,
+        marginTop: 10,
+    },
+    left: {
+        flexDirection: "row",
+        width: 120,
+        justifyContent: "space-between",
+    },
+    likes: {
+        color: "white",
+        marginLeft: 10,
+    },
+    caption: {
+        color: "white",
+        marginLeft: 10,
+        marginVertical: 3,
+    },
+});
--- TecDeProgramacion/src/screens/Home.jsx
+++ TecDeProgramacion/src/screens/Home.jsx
@@ -1,47 +1,48 @@
 import React, { useEffect } from "react";
 import { Text, View, StyleSheet, FlatList } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, FontAwesome5 } from "@expo/vector-icons";
 import Story from "../components/Story";
+import Post from "../components/Post";
 
 const STORIES = [
     {
         id: 1,
-        name: "Eula",
-        image: "https://i.waifu.pics/jIClgKA.jpg",
+        name: "Ganyu",
+        image: "https://i.waifu.pics/dPXxQqE.png",
     },
     {
         id: 2,
         name: "Amber",
-        image: "https://i.waifu.pics/_zlfBgp.jpg",
+        image: "https://i.waifu.pics/-ABlAvr.jpg",
     },
     {
         id: 3,
         name: "Diluc",
-        image: "https://i.waifu.pics/4Ni15Gm.jpg",
+        image: "https://i.waifu.pics/8TL6ycS.jpg",
     },
     {
         id: 4,
         name: "Jean",
-        image: "https://i.waifu.pics/4Ni15Gm.jpg",
+        image: "https://i.waifu.pics/-eOx7uI.jpg",
     },
     {
         id: 5,
-        name: "Eula",
-        image: "https://i.waifu.pics/jIClgKA.jpg",
+        name: "Kazuha",
+        image: "https://i.waifu.pics/q7xF_wS.png",
     },
     {
         id: 6,
-        name: "Amber",
-        image: "https://i.waifu.pics/_zlfBgp.jpg",
+        name: "Zhongli",
+        image: "https://i.waifu.pics/fuGfYQJ.jpg",
     },
     {
         id: 7,
-        name: "Diluc",
-        image: "https://i.waifu.pics/4Ni15Gm.jpg",
+        name: "Ayaka",
+        image: "https://i.waifu.pics/vzh9Dhm.jpg",
     },
     {
         id: 8,
-        name: "Jean",
+        name: "Mona",
         image: "https://i.waifu.pics/4Ni15Gm.jpg",
     },
 ];
@@ -49,35 +50,35 @@ const STORIES = [
 const POSTS = [
     {
         id: 1,
-        profilePic: "https://i.waifu.pics/jIClgKA.jpg",
-        name: "Eula",
-        image: "https://i.waifu.pics/jIClgKA.jpg",
+        profilePic: "https://i.waifu.pics/3x~ovC6.jpg",
+        name: "Ganyu",
+        image: "https://i.waifu.pics/mbQ4c4V.jpg",
         likes: 100,
-        caption: "This is a caption",
+        caption: "Dale like si te gustan las clases de Eder",
     },
     {
         id: 2,
-        profilePic: "https://i.waifu.pics/jIClgKA.jpg",
-        name: "Eula",
-        image: "https://i.waifu.pics/jIClgKA.jpg",
+        profilePic: "https://i.waifu.pics/8hqFjfL.png",
+        name: "Keqing",
+        image: "https://i.waifu.pics/6NYYSw6.png",
         likes: 100,
-        caption: "This is a caption",
+        caption: "Denme primo gemas",
     },
     {
         id: 3,
         profilePic: "https://i.waifu.pics/jIClgKA.jpg",
-        name: "Eula",
-        image: "https://i.waifu.pics/jIClgKA.jpg",
+        name: "Amber",
+        image: "https://i.waifu.pics/mUTwf4z.jpg",
         likes: 100,
-        caption: "This is a caption",
+        caption: "With my friends",
     },
     {
         id: 4,
         profilePic: "https://i.waifu.pics/jIClgKA.jpg",
         name: "Eula",
-        image: "https://i.waifu.pics/jIClgKA.jpg",
+        image: "https://i.waifu.pics/sLd~4NU.jpg",
         likes: 100,
-        caption: "This is a caption",
+        caption: "Genshin Impact",
     },
 ];
 
@@ -88,7 +89,7 @@ const Home = () => {
                 <Text style={{ fontSize: 20, color: "#fff" }}>Instagram</Text>
                 <View style={styles.icons}>
                     <AntDesign name="hearto" size={24} color="white" />
-                    <AntDesign name="message1" size={24} color="white" />
+                    <FontAwesome5 name="facebook-messenger" size={24} color="white" />
                 </View>
             </View>
             <View style={{ flexDirection: "row" }}>
@@ -102,7 +103,22 @@ const Home = () => {
                 />
             </View>
             <View>
-                <FlatList />
+                <FlatList
+                    data={POSTS}
+                    keyExtractor={({ id }) => id.toString()}
+                    vertical
+                    showsVerticalScrollIndicator={false}
+                    renderItem={({ item }) => (
+                        <Post
+                            profilePic={item.profilePic}
+                            name={item.name}
+                            image={item.image}
+                            likes={item.likes}
+                            caption={item.caption}
+                        />
+                    )}
+                    ListFooterComponent={() => <View style={{ marginBottom: 120 }} />} // Para que no se corte el último post
+                />
             </View>
         </View>
     );
